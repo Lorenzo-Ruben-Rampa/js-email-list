9f9ea6bcf24f610ca5638e5944567f05a72f4f19
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-22 16:57:56 +0100

    tentativo n1

--- js/script.js
+++ js/script.js
@@ -8,4 +8,15 @@ const emailList = [];
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 //Richiesta Ajax per generare email
-axios.get(endpoint);
+axios.get(endpoint)
+    .then(response => {
+        for(let i=0; i <= 10; i++) {
+            let randomEmail = emailList[i];
+            const newRandomEmail = emailList.push; 
+        }
+    })
+
+console.log(randomEmail);
+
+// // Output in pagina
+// outputEl.innerText = email;
